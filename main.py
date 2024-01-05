c18574689e4f9bbf55155802fe462f79a759559f
Tasdik <mtasdikyousuf@gmail.com>
2024-01-05 18:11:07 +0600

    Introducing TK

--- main.py
+++ main.py
@@ -1,52 +1,135 @@
-def calculate_bmi(weight, height):
-    """
-    Calculate Body Mass Index (BMI) based on weight (in kilograms) and height (in meters).
+from tkinter import *
+from tkinter import messagebox
 
-    Parameters:
-    - weight (float): Body mass in kilograms.
-    - height (float): Height in meters.
 
-    Returns:
-    - float: Calculated BMI.
-    """
-    bmi = weight / (height ** 2)
-    return bmi
+def reset():
+    age_tf.delete(0, 'end')
+    height_tf.delete(0, 'end')
+    weight_tf.delete(0, 'end')
 
-def get_bmi_category(bmi):
-    """
-    Determine BMI category based on the calculated BMI.
 
-    Parameters:
-    - bmi (float): Calculated BMI.
+def calculate_bmi():
+    weight_in_kilograms = int(weight_tf.get())
+    height_in_meters = int(height_tf.get()) / 100
+    bmi = weight_in_kilograms / (height_in_meters ** 2)
+    bmi = round(bmi, 2)
+    bmi_index(bmi)
 
-    Returns:
-    - str: BMI category (e.g., "Underweight", "Normal", "Overweight", "Obese", "Morbidly Obese").
-    """
-    if bmi <= 18.5:
-        return "Underweight"
-    elif bmi <= 25:
-        return "Normal"
-    elif bmi <= 30:
-        return "Overweight"
-    elif bmi <= 35:
-        return "Obese"
+
+def bmi_index(bmi):
+    if bmi < 18.5:
+        messagebox.showinfo('Underweight', f'BMI = {bmi}\n Category: Underweight')
+    elif (bmi > 18.5) and (bmi < 24.9):
+        messagebox.showinfo('Normal', f'BMI = {bmi}.\n Category: Normal')
+    elif (bmi > 24.9) and (bmi < 29.9):
+        messagebox.showinfo('Overweight', f'BMI = {bmi}.\n Category: Overweight')
+    elif (bmi > 29.9):
+        messagebox.showinfo('Obese', f'BMI = {bmi}.\n Category: Obese')
+    elif (bmi > 34.9):
+        messagebox.showinfo('Morbidly Obese', f'BMI = {bmi}.\n Category: Morbidly Obese')
     else:
-        return "Morbidly Obese"
+        messagebox.showerror('Invalid!', 'Enter correct data')
 
-def bmi_calculator():
-    """
-    Collect user input for weight and height, calculate BMI, and display the BMI category.
 
-    Returns:
-    - None: Prints the calculated BMI and BMI category.
-    """
-    weight = float(input("Enter body mass in kilograms: "))
-    height = float(input("Enter height in meters: "))
+ws = Tk()
+ws.title('BMI Calculator')
+ws.geometry('1200x900')
+ws.config(bg='#020803')
 
-    bmi = calculate_bmi(weight, height)
-    bmi_category = get_bmi_category(bmi)
+var = IntVar()
 
-    print("BMI:", round(bmi, 2))
-    print("Category:", bmi_category)
+frame = Frame(
+    ws,
+    padx=10,
+    pady=10
+)
+frame.pack(expand=True)
 
-bmi_calculator()
+age_lb = Label(
+    frame,
+    text="Enter Age (2 - 120)"
+)
+age_lb.grid(row=1, column=1)
+
+age_tf = Entry(
+    frame,
+)
+age_tf.grid(row=1, column=2, pady=5)
+
+gen_lb = Label(
+    frame,
+    text='Select Gender'
+)
+gen_lb.grid(row=2, column=1)
+
+frame2 = Frame(
+    frame
+)
+frame2.grid(row=2, column=2, pady=5)
+
+male_rb = Radiobutton(
+    frame2,
+    text='Male',
+    variable=var,
+    value=1
+)
+male_rb.pack(side=LEFT)
+
+female_rb = Radiobutton(
+    frame2,
+    text='Female',
+    variable=var,
+    value=2
+)
+female_rb.pack(side=RIGHT)
+
+height_lb = Label(
+    frame,
+    text="Enter Height (cm)  "
+)
+height_lb.grid(row=3, column=1)
+
+weight_lb = Label(
+    frame,
+    text="Enter Weight (kg)  ",
+
+)
+weight_lb.grid(row=4, column=1)
+
+height_tf = Entry(
+    frame,
+)
+height_tf.grid(row=3, column=2, pady=5)
+
+weight_tf = Entry(
+    frame,
+)
+weight_tf.grid(row=4, column=2, pady=5)
+
+frame3 = Frame(
+    frame
+)
+frame3.grid(row=5, columnspan=3, pady=10)
+
+cal_btn = Button(
+    frame3,
+    text='Calculate',
+    command=calculate_bmi
+)
+cal_btn.pack(side=LEFT)
+
+reset_btn = Button(
+    frame3,
+    text='Reset',
+    command=reset
+)
+reset_btn.pack(side=LEFT)
+
+exit_btn = Button(
+    frame3,
+    text='Exit',
+    command=lambda: ws.destroy()
+)
+exit_btn.pack(side=RIGHT)
+
+ws.mainloop()
